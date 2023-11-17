572b822d87e0350e175262b3702c582540339466
Ann S Johnson <142471577+rubyann2000@users.noreply.github.com>
2023-11-17 14:13:23 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,1 +1,8 @@
+import datetime
+import time
 
+def get_user_input():
+    name = input("Enter your name: ")
+    medication = input("Enter the name of your medication: ")
+    dosage = input("Enter the dosage of your medication: ")
+    return name, medication, dosage
