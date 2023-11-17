af87f83a3ae7a66ee3c318be77031269417157ad
Ann S Johnson <142471577+rubyann2000@users.noreply.github.com>
2023-11-17 14:14:16 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -6,3 +6,7 @@ def get_user_input():
     medication = input("Enter the name of your medication: ")
     dosage = input("Enter the dosage of your medication: ")
     return name, medication, dosage
+    
+def set_reminder(name, medication, dosage, reminder_time):
+    print(f"\nReminder set for {reminder_time.strftime('%H:%M')}:")
+    print(f"Hello, {name}! Don't forget to take your {medication} ({dosage}).")
