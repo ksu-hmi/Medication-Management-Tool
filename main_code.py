621bf4c258bed42a1be17d34a839425c173ffae7
Ann S Johnson <142471577+rubyann2000@users.noreply.github.com>
2023-11-18 14:58:15 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,16 +1,3 @@
-import datetime
-import time
-
-def get_user_input():
-    name = input("Enter your name: ")
-    medication = input("Enter the name of your medication: ")
-    dosage = input("Enter the dosage of your medication: ")
-    return name, medication, dosage
-    
-def set_reminder(name, medication, dosage, reminder_time):
-    print(f"\nReminder set for {reminder_time.strftime('%H:%M')}:")
-    print(f"Hello, {name}! Don't forget to take your {medication} ({dosage}).")
-
 # Medication Management Tool
 # Base code from found repository to build upon 
 
