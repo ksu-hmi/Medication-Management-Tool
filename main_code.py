bb738d8452abc0105ca3c01074bd644029fd7bf4
calisehammonds <142542426+calisehammonds@users.noreply.github.com>
2023-11-17 19:58:27 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -13,130 +13,232 @@ def set_reminder(name, medication, dosage, reminder_time):
 
 # Medication Management Tool
 # Base code from found repository to build upon 
-import datetime, os, sys, threading, yaml
-from gi.repository import Gtk
-import BuilderObject, DatePicker, PillEdit, TemplateObjects
-
-if sys.platform == 'linux':
-    import notifications.LinuxNotifications as Notifications
-elif sys.platform == 'win32':
-    import notifications.Win32Notifications as Notifications
-elif sys.platform == 'darwin':
-    import notifications.DarwinNotifications as Notifications
-else:
-    import notifications.DummyNotifications as Notifications
-
-SaveDataFilename = "{}/savedData/pills.yaml".format(sys.path[0])
-
-class Tracker(BuilderObject.BuilderObject):
-    def __init__(self):
-        super().__init__("tracker")
-        self.window = self.builder.get_object("tracker_window")
-        self.date_label = self.builder.get_object("date_label")
-        self.pills_container = self.builder.get_object("pills_container")
-        self.no_pills_label = self.builder.get_object("no_pills_label")
-        self.date = None
-        self.pills = []
-        self.pill_to_widget = {}
-        self.load_pills()
-        self.set_date(datetime.date.today())
-        self.launch_time_check_thread(0.1)
-
-    def on_tracker_window_destroy(self, widget):
-        Gtk.main_quit()
-
-    def on_add_pill_button_clicked(self, widget):
-        pill_edit = PillEdit.PillEdit(self)
-        pill_edit.new_mode()
-        pill_edit.window.show_all()
-
-    def on_date_menu_select_activate(self, widget):
-        date_picker = DatePicker.DatePicker(self)
-        date_picker.calendar.select_month(self.date.month - 1, self.date.year)
-        date_picker.calendar.select_day(self.date.day)
-        date_picker.window.show_all()
-
-    def on_date_menu_today_activate(self, widget):
-        self.set_date(datetime.date.today())
-
-    def set_date(self, date):
-        self.date = date
-        self.date_label.set_text(date.strftime("%A %x"))
-        for pill in self.pills:
-            widget = self.pill_to_widget.get(pill, None)
-            if widget is None:
-                continue
-            for time_tracker in widget.time_trackers.values():
-                dates_taken = pill.dates_taken[time_tracker.time.get_hour_minute()]
-                time_tracker.checkbox.set_active(self.date in dates_taken)
-        self.save_pills()
+# Prescription Reminder
+~
+~
+#Importing all the necessary libraries:
+from tkinter import *
+import datetime
+import time
+import sys
+import signal
+import os
+import webbrowser
+import time
+import re
 
 
-    def add_pill(self, pill):
-        self.pills_container.remove(self.no_pills_label)
-        self.pills.append(pill)
-        widget = TemplateObjects.PillFrame(pill, self)
-        self.pills_container.add(widget.frame)
-        self.pill_to_widget.update({pill: widget})
-        self.save_pills()
+# Log into the app
+print ("RemindRx, your guide to better health")
 
-    def delete_pill(self, pill):
-        try:
-            self.pills.remove(pill)
-        except ValueError:
-            # It might have already been removed due to some race condition.
-            print("Couldn't remove pill {} from tracker.pills!".format(pill.name), file = sys.stderr)
-        pillWidget = self.pill_to_widget.pop(pill, None) # Get the pill's widget, if it exists
-        if pillWidget is not None: # If there was a widget, remove it
-            self.pills_container.remove(pillWidget.frame)
-            pillWidget = None
-        if len(self.pills) == 0:
-            self.pills_container.pack_start(self.no_pills_label, True, True, 0)
-        self.save_pills()
+print("Enter Email address(Username): ")
+email_address = input()
+while "@" not in email_address:
+    email_address = input("Your email address must have '@' in it\nPlease write your email address again: ")
+    if len(email_address) <= 6 :
+        email_address = input("Your email address is too short\nPlease write your email address again: ")
+    if "." not in email_address:
+        email_address = input("Your email address must have '.' in it\nPlease write your email address again: ")
+while "." not in email_address:
+    email_address = input("Your email address must have '.' in it\nPlease write your email address again: ")
+    if len(email_address) <= 6 :
+        email_address = input("Your email address is too short\nPlease write your email address again: ")
+    if "@" not in email_address:
+        email_address = input("Your email address must have '@' in it\nPlease write your email address again: ")
 
-    def save_pills(self):
-        if not os.path.isdir(os.path.dirname(SaveDataFilename)):
-            os.mkdir(os.path.dirname(SaveDataFilename), 0o755)
-        with open(SaveDataFilename, 'w') as save_data:
-            yaml.dump(self.pills, save_data)
+# Create password
+def checkPassword(password):
+    """
+    Validate the password
+    """
+    if len(password) < 8:
+        # Password to short
+        print("Your password must be 8 characters long.")
+        return False
+    elif not re.findall(r'\d+', password):
+        # Missing a number
+        print("You need a number in your password.")
+        return False
+    elif not re.findall(r'[A-Z]+', password):
+        # Missing at least one capital letter
+        print("You need a capital letter in your password.")
+        return False
+    else:
+        # All good
+        print("All good")
+        return True
 
-    def load_pills(self):
-        if os.path.isfile(SaveDataFilename): # only try if it really exists
-            with open(SaveDataFilename, 'r') as save_data:
-                for pill in yaml.safe_load(save_data):
-                    self.add_pill(pill)
+# Promt user to enter valid password
+passwordValid = False
+while not passwordValid:
+    create_password = input( "Create your password:\n (Password must contain at least 8 characters, one number,\n one capital letter, and one lowercase letter)")
+    passwordValid = checkPassword(create_password)
 
-    def time_check(self):
-        today = datetime.date.today()
-        if self.date != today and today - datetime.timedelta(days = 1) == self.date:
-            self.set_date(today)
+print("Enter your first name: ")
+first_name=input()
+print("Enter your last name: ")
+last_name=input()
 
-        now = datetime.datetime.now().time()
-        for pill in self.pills:
-            widget = self.pill_to_widget.get(pill)
-            for time in pill.times:
-                time_tracker = None
-                if widget is not None:
-                    time_tracker = widget.time_trackers.get(time.get_hour_minute())
-                if now >= time.get_datetime_time():
-                    if today in pill.dates_taken[time.get_hour_minute()]:
-                        # We've taken this pill.
-                        if time_tracker is not None:
-                            time_tracker.alert.set_visible(False)
-                    else:
-                        # uh oh!
-                        if time.notifications and not time_tracker.alert.get_visible():
-                            # send a notification!
-                            Notifications.send_notification("Pill Tracker", "It's time for your {} dose of {}!".format(time.name, pill.name))
-                        if time_tracker is not None:
-                            time_tracker.alert.set_visible(True)
-                else:
-                    # It's not time to take this yet.
-                    if time_tracker is not None:
-                        time_tracker.alert.set_visible(False)
-        self.launch_time_check_thread(0.25)
+#Now log in with new password
+print("Welcome", first_name + "!", "Your profile is set up")
+print("Log in to access the application\n You have 3 attempts or application quits")
 
-    def launch_time_check_thread(self, time):
-        thread = threading.Timer(time, self.time_check)
-        thread.daemon = True
-        thread.start()
+attempts = 0
+
+username = email_address
+password = create_password
+
+while True:
+    usern = input("Enter Username: ")
+    print()
+    userp = input("Enter Password: ")
+    attempts += 1
+    if attempts == 3:
+        print("Too many incorrect attempts. Please try again in few minutes")
+        exit()
+    else:
+        if usern == username and userp == password:
+            print("\nAccess Granted. Welcome " + first_name)
+            break
+        else:
+            print("\nIncorrect credentials. Try again")
+            
+
+
+def print_menu():
+    print()
+    print("Welcome to RemindRx! Press enter to select option")
+    print()
+
+    choice = input("""
+            1. Add A Medication
+            2. Delete A Medication
+            3. Review Your Medication List
+            4. Set an Alarm for Medication
+            5. Exit
+            """)
+
+print_menu()
+
+
+choice = input("Select the menu item that you want edit [1-5]: ")
+choice = int(choice)
+medication_name=[]
+
+while choice != 5:
+    if choice == 1:
+        med_add = input("Enter the medication Name to add to your list: ")
+        medication_name.append(med_add)
+        print("Updated Medication List: ", medication_name) 
+        med_direction = input()
+        break
+    elif choice == 2:
+        print ("Delete A Medication")
+        med_remove = input("Enter the medication that you are removing from your list: ")   
+        medication_name.remove(med_remove)
+        print("Updated medication list: ", medication_name)
+        continue
+    elif choice == 3:
+        print ("Review Your Medication List")
+        print("Current medication list: ", "\n", medication_name)
+        break
+    elif choice == 4:
+        print ("Set an Alarm for Medication")
+
+        alarm_HH = input("Enter the hour you want to take the medication - in 24 hour format: ")
+        alarm_MM = input("Enter the minute you want to take the medication: ")
+
+
+print(medication_name)
+
+print("Enter the directions for medication : ")
+med_direction = input()
+
+print(med_direction)
+#need input format for the time so end user will know how to enter it!
+print("Enter time of day medication is to be taken : ") 
+time_take=input('Please input the time for the alarm in format HHMM: \n ')
+print(time_take)
+print("Hello", first_name,"!", "Remember to take", med_direction, "at", time_take)
+
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print("The Set Date is:",date)
+        print(now)
+        if now == set_alarm_timer:
+            print("Time to take med")
+        winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+        break
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+    
+   
+# Create GUI in tinker
+clock = Tk()
+clock.title("RemindRx")
+clock.geometry("400x200")
+time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
+addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
+setYourAlarm = Label(clock,text = "Remember to take your med",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
+# The Variables we require to set the alarm(initialization):
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+#Time required to set the alarm clock:
+hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=110,y=30)
+minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=150,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=30)
+#To take the time input by user:
+submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
+clock.mainloop()
+#Execution of the window.
+~
+~
+~
+~
+~
+# Prescription Refill Reminder
+import datetime
+import decimal
+InputDatePrescribed= input("When was prescription written? (year-month-day): ")
+InputPillPatchRefillCount = input("How many pills/inhalers in this refill?: ")
+InputPillsPerday = input("How many pills can be taken in a day?: ")
+InputPatchDuration = input("How often is the inhaler used (hours)?: ")
+# Written in standard Year, month,day 2008-8-2
+YearMonthDayInput = InputDatePrescribed.split('-')#split this in arguments using"-", then input that
+InputYear,InputMonth,InputDay = InputDatePrescribed.split('-')
+DatePrescribed = datetime.date (int(InputYear), int(InputMonth),int(InputDay))
+
+
+def PillRefillDuration (InputPillPatchRefillCount,InputPillsPerday): #STATES HOW MANY DAYS IT SHOULD LAST AS READABLE OUTPUT
+    PillRefillDuration = decimal.Decimal(decimal.Decimal(InputPillPatchRefillCount)/decimal.Decimal(InputPillsPerday))
+    return PillRefillDuration
+
+def PillDeltaDueDate (InputPillPatchRefillCount, InputPillsPerday): #RETURNS A TIMEDELTA OBJECT IN DAYS WHICHS ALLOWS MANIPULATION
+    return datetime.timedelta (float(PillRefillDuration(InputPillPatchRefillCount, InputPillsPerday)))#Need to do this since timedelta doesn't take Decimal type
+    
+def PatchRefillDuration (InputPillPatchRefillCount,InputPatchDuration):
+    PatchRefillDuration = decimal.Decimal(decimal.Decimal(InputPatchDuration)/decimal.Decimal(24)) *decimal.Decimal(InputPillPatchRefillCount)
+    return PatchRefillDuration
+
+def PatchDeltaDueDue (InputPillPatchRefillCount, InputPatchDuration):
+    return datetime.timedelta (float (PatchRefillDuration(InputPillPatchRefillCount, InputPatchDuration)))
+    
+if InputPatchDuration == "":
+    a = PillRefillDuration (InputPillPatchRefillCount, InputPillsPerday)
+    print ("It shoud last ", a, " days.")
+    print ("It was prescrbied", DatePrescribed)
+    DueDate = DatePrescribed + PillDeltaDueDate (InputPillPatchRefillCount, InputPillsPerday)
+    print ("It is due on", DueDate)
+
+if InputPillsPerday == "":
+    a = PatchRefillDuration(InputPillPatchRefillCount, InputPatchDuration)
+    print ("It shoud last ", a, " days.")
+    print ("It was prescrbied", DatePrescribed)
+    DueDate = DatePrescribed + PatchDeltaDueDue (InputPillPatchRefillCount, InputPatchDuration)
+    print ("It is due on", DueDate)
