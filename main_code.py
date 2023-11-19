73f1c2cc5721cee2df4af8e4e6b3f94af465b5f5
Ann S Johnson <142471577+rubyann2000@users.noreply.github.com>
2023-11-18 21:37:01 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -161,11 +161,17 @@ view_button.pack()
 
 while choice != 7:
     if choice == 1:
-        med_add = input("Enter the medication Name to add to your list: ")
-        medication_name.append(med_add)
-        print("Updated Medication List: ", medication_name) 
-        med_direction = input()
-        break
+        # Defining and appending variables
+        medication_name = input('Enter the name of your medication: ')
+        dosage = input('Enter the dosage of your medication: ')
+        directions = input('Enter directions for your medication intake: ')
+        
+        medication_names.append(medication_name)
+
+        print('Medication Name:', medication_name)
+        print('Dosage:', dosage)
+        print('Directions:', directions)
+
     elif choice == 2:
         print ("Delete A Medication")
         med_remove = input("Enter the medication that you are removing from your list: ")   
